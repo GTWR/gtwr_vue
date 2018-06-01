aeacf56d7aee1a582508044293d58ce540a2bf25
ZhuZhaoY <1547480231@qq.com>
2018-06-01 23:24:35 +0800

    update

--- src/components/coverForlogin.vue
+++ src/components/coverForlogin.vue
@@ -6,11 +6,11 @@
         <header class="login-title"><b>欢迎登录</b></header>
         <div class="login">
             <input type="text" placeholder="输入用户名..."><br/>
-            <input type="text" placeholder="输入密码"><br/>
+            <input type="password" placeholder="输入密码"><br/>
             <button @click="submitPar" ><span>登录</span></button>
             <div>
               <a style="float:right">忘记密码？</a>
-              <a style="float:left">注册</a>
+              <a  href="#/coverForSign"  @click="submitPar"  style="float:left">注册</a>
             </div>
         </div> 
       </div>
@@ -22,12 +22,11 @@
 <script>
 require('../style/coverLogin.scss')
 import {mapState} from 'vuex'
-
 export default {
-  name: 'coverLogin',
+	  name: 'coverLogin',
   data () {
     return {
-      
+      visible:false
     }
   },
   mounted(){
@@ -40,10 +39,11 @@ export default {
     })
   },
   methods:{
-    submitPar:function(){
+      submitPar:function(){
       //关闭遮罩层与登录面板
       this.$store.dispatch('LoginShowAction',false);
-    },
+      },
+      
   }
 }
 </script>
