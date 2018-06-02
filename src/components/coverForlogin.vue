77d7a0a153db75248c92e9581dd5467ac13b777f
zhangmingemma <zhangmin_gemma@163.com>
2018-06-02 13:29:44 +0800

    zm

--- src/components/coverForlogin.vue
+++ src/components/coverForlogin.vue
@@ -2,18 +2,18 @@
   <div class="cover-login">
     <div class="box-container">
       <div class="main">
-        <hr color="lightgrey"/>
-        <header class="login-title"><b>欢迎登录</b></header>
-        <div class="login">
-            <input type="text" placeholder="输入用户名..."><br/>
-            <input type="text" placeholder="输入密码"><br/>
-            <button @click="submitPar" ><span>登录</span></button>
-            <div>
-              <a style="float:right">忘记密码？</a>
-              <a style="float:left">注册</a>
-            </div>
-        </div> 
+  <hr color="lightgrey"/>
+  <header class="login-title"><b>欢迎登录</b></header>
+  <div class="login">
+      <input type="text" placeholder="输入用户名..."><br/>
+      <input type="text" placeholder="输入密码"><br/>
+      <button @click="submitPar" ><span>登录</span></button>
+      <div>
+        <a style="float:right">忘记密码？</a>
+        <a style="float:left">注册</a>
       </div>
+  </div> 
+</div>
     </div>
     <img src="../assets/img/close.png" height="20" width="20" @click="submitPar">
   </div>
@@ -21,7 +21,6 @@
 
 <script>
 require('../style/coverLogin.scss')
-import {mapState} from 'vuex'
 
 export default {
   name: 'coverLogin',
@@ -31,19 +30,19 @@ export default {
     }
   },
   mounted(){
-    
+    //this.$router.push({path:'/login/loginIn'});
   },
   computed:{
-    ...mapState({
-        // ...
-      
-    })
+
   },
   methods:{
     submitPar:function(){
       //关闭遮罩层与登录面板
       this.$store.dispatch('LoginShowAction',false);
     },
+  },
+  components:{
+
   }
 }
 </script>
