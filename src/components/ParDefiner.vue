b355224a744384ca83f21d3d1d3a9370b1230cbe
zhangmingemma <zhangmin_gemma@163.com>
2018-10-24 17:41:26 +0800

    参数设置更改为输入+select；开始计算按钮动画偏移；参数设置模块样式调整

--- src/components/ParDefiner.vue
+++ src/components/ParDefiner.vue
@@ -5,34 +5,30 @@
 			<button :class="{active: tab==1}" @click="gtwr">GTWR</button>
 			<button :class="{active: tab==2}" @click="gwr">GWR</button>
 		</div>
-
-		<div class="par-list">
-		   
+		<div class="par-list">	   
 			 <div v-if="tab==1">
 		     <div v-for="(par,index) in par_list1">
-		    	   <span style="display:inline-block;width:80px;text-align:left;">{{par.name}}：</span><input placeholder="输入参数" v-model="par.value" >
-		        	<p style="font-size:14px;">参数说明：{{par.content}}</p>
+		    	  <span class="par-title">{{par.name}}：</span>
+						<input placeholder="输入参数" v-model="par.value" >
+						<select v-model="par.value"><option v-for="item in varList">{{item}}</option></select>
+		        <p>参数说明：{{par.content}}</p>
 	   	   </div>
 			 </div>
-
 			 <div v-else>
-		    	   <div v-for="(par,index) in par_list2">
-		    	   <span style="display:inline-block;width:80px;text-align:left;">{{par.name}}：</span><input placeholder="输入参数" v-model="par.value" >
-		        	<p style="font-size:14px;">参数说明：{{par.content}}</p>
+						<div v-for="(par,index) in par_list2">
+						<span class="par-title">{{par.name}}：</span>
+						<input placeholder="输入参数" v-model="par.value" >
+						<select v-model="par.value"><option v-for="item in varList">{{item}}</option></select>
+		        <p>参数说明：{{par.content}}</p>
 	   	   </div>
-	   	 </div>
-		  
+	   	 </div>		  
 			<div style="background:white;width:100px;height:18px;border:none;margin-left: 206px">
-			<div id="proBar" style="height:30px;width:0%;background-color:rgba(0,0,0,0.8);color:rgba(0,0,0,0.8);border-radius: 3px;">
-				<span style="display: inline-block;position: relative;">
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>
-			</div>
-		</div> 
-		<button class="submit" style="vertical-align:middle" @click="compute"  ><span>开始计算</span></button> 
-		
-	</div>
-	   
-    
+				<div id="proBar" style="height:30px;width:0%;background-color:rgba(0,0,0,0.8);color:rgba(0,0,0,0.8);border-radius: 3px;margin-left: 18px;">
+					<span style="display: inline-block;position: relative;"></span>
+				</div>
+			</div> 
+		<button class="submit" style="vertical-align:middle" @click="compute"><span>开始计算</span></button> 		
+	</div>  
   </div>
 </template>
 
@@ -65,6 +61,15 @@ export default {
     }
   },
   mounted(){
+	},
+	computed:{
+    ...mapState({
+      parentNodeIndex: state => state.parent_node_index,
+      childNodeIndex: state => state.child_node_index,
+    }),
+    varList(){
+      return Object.keys(this.$store.state.data_list[this.parentNodeIndex].data_type[this.childNodeIndex].url[0].features[0].properties);
+    },
   },
   methods:{
   	compute:function(){
